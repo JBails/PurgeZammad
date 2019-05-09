895b8d446cb9a6aef4c262bc4902eec33be443d9
JBails <39381336+JBails@users.noreply.github.com>
2019-05-09 16:56:24 +0100

    Update purgezammadcustomersticketsolder30days.rb

--- purgezammadcustomersticketsolder30days.rb
+++ purgezammadcustomersticketsolder30days.rb
@@ -1,6 +1,6 @@
 # THIS SCRIPT WILL DESTROY CUSTOMERS HAVE CLOSED TICKETS OLDER THAN 30 DAYS, IGNORING MXDATA AND MAPWAY RELATED USERS
 
-ticketless_customers = User.with_permissions('ticket.customer').where('email NOT LIKE ?', '%mapway.com%').where('email NOT LIKE ?', '%mxdata.co.uk%').where('updated_at < NOW() - INTERVAL 30 DAY;')
+ticketless_customers = User.with_permissions('ticket.customer').where('email NOT LIKE ?', '%mapway.com%').where('email NOT LIKE ?', '%mxdata.co.uk%').where('updated_at < DATEADD(dd,-30,GETDATE())')
 count = ticketless_customers.count
 
 puts "#{count} customers without current tickets found, but tickets older than 30 days."
