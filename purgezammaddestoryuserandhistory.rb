b48ddf81378c2389f5cdc69b202589e3769b8687
JBails <39381336+JBails@users.noreply.github.com>
2019-05-14 11:38:06 +0100

    Update purgezammaddestoryuserandhistory.rb

--- purgezammaddestoryuserandhistory.rb
+++ purgezammaddestoryuserandhistory.rb
@@ -1,24 +1,42 @@
-# Actual deletion, requires overview run before
-User.joins(roles: :permissions).where(email: target_user_emails.map(&:downcase), roles: { active: true }, permissions: { name: 'ticket.customer', active: true }).where.not(id: 1).where('email NOT LIKE ?', '%mapway.com%').where('email NOT LIKE ?', '%mxdata.co.uk%').where("updated_at < NOW() - INTERVAL '30' DAY").find_each do |user|
- puts "Customer #{user.login}/#{user.email} has #{Ticket.where(customer_id: user.id).count} tickets"
+# THIS SCRIPT WILL DESTROY CUSTOMERS HAVE CLOSED TICKETS OLDER THAN 30 DAYS, IGNORING MXDATA AND MAPWAY RELATED USERS
 
- Ticket.where(customer: user).find_each do |ticket|
-   puts "  Deleting ticket #{ticket.number}..."
-   ticket.destroy
- end
+ticketless_customers = User.with_permissions('ticket.customer').where('email NOT LIKE ?', '%mapway.com%').where('email NOT LIKE ?', '%mxdata.co.uk%').where("updated_at < NOW() - INTERVAL '30' DAY")
+count = ticketless_customers.count
 
- puts "  Removing references for user with E-Mail #{user.email}..."
- ActivityStream.where(created_by_id: user.id).update_all(created_by_id: 1)
- History.where(created_by_id: user.id).update_all(created_by_id: 1)
- Ticket::Article.where(created_by_id: user.id).update_all(created_by_id: 1)
- Ticket::Article.where(updated_by_id: user.id).update_all(updated_by_id: 1)
- Store.where(created_by_id: user.id).update_all(created_by_id: 1)
- StatsStore.where(created_by_id: user.id).update_all(created_by_id: 1)
- Tag.where(created_by_id: user.id).update_all(created_by_id: 1)
- if OnlineNotification.find_by(user_id: user.id)==""
-  OnlineNotification.find_by(user_id: user.id).destroy!
- end
+puts "#{count} customers without current tickets found, but with tickets older than 30 days."
+puts
 
- puts "  Deleting user #{user.login}/#{user.email}..."
- user.destroy
+ticketless_customers.find_each.with_index do |user, i|
+  next if user.permissions?(%w[admin ticket.agent])
+  next if user.id == 1
+
+  display_name = user.fullname + (user.fullname == user.email ? '' : " (#{user.email})")
+
+  Ticket.where(customer: user).find_each do |ticket|
+    puts "  Deleting ticket #{ticket.number}..."
+    ticket.destroy
+  end
+
+  puts "  Removing references for user with E-Mail #{user.email}..."
+  ActivityStream.where(created_by_id: user.id).update_all(created_by_id: 1)
+  History.where(created_by_id: user.id).update_all(created_by_id: 1)
+  Ticket::Article.where(created_by_id: user.id).update_all(created_by_id: 1)
+  Ticket::Article.where(updated_by_id: user.id).update_all(updated_by_id: 1)
+  Store.where(created_by_id: user.id).update_all(created_by_id: 1)
+  StatsStore.where(created_by_id: user.id).update_all(create
+  Tag.where(created_by_id: user.id).update_all(created_by_id: 1)
+  if OnlineNotification.find_by(user_id: user.id)==""
+    OnlineNotification.find_by(user_id: user.id).destroy!
+  end
+  
+  User.transaction do
+    begin
+      user.destroy!
+      puts "  #{display_name}/#{user.email} deleted."
+    rescue => e
+      puts "  #{display_name}/#{user.email} could not be deleted: #{e.message}"
+      raise ActiveRecord::Rollback
+   next
+    end
+  end
 end
