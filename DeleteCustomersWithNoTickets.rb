02c4bf8d2990b676f7d1bfe66de80d01bb47622f
JBails <39381336+JBails@users.noreply.github.com>
2019-05-17 11:28:25 +0100

    Update and rename purgezammadcustomersnotickets.rb to DeleteCustomersWithNoTickets.rb

--- DeleteCustomersWithNoTickets.rb
+++ DeleteCustomersWithNoTickets.rb
@@ -1,3 +1,6 @@
+# This script will delete all users that do not have an assigned ticket (open or closed), with no regards as to how long they haven't had a ticket
+# i.e. doesn't care if the last ticket was deleted 30 seconds or 30 days ago
+
 ticketless_customers = User.with_permissions('ticket.customer').where('email NOT LIKE ?', '%mapway.com%').where('email NOT LIKE ?', '%mxdata.co.uk%').where('id NOT IN (SELECT customer_id FROM tickets)')
 count = ticketless_customers.count
 
